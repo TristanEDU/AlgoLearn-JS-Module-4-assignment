a2263f9eb27d1a5c0739009fa81c60bbac491548
Tristan <tjtedu@protonmail.com>
2026-06-11 15:17:51 -0700

    final commit

--- todo.js
+++ todo.js
@@ -42,6 +42,43 @@ async function app() {
     utils.writeFile();
     app();
   } else if (input === 4) {
+    utils.printTasks();
+    choice = await rl.viewTasksMenu();
+    input = Number(choice);
+    if (Number.isNaN(input) || input < 0 || input > 4) {
+      console.error(
+        `Error, ${input} is not a valid option. Please choose  0, 1, 2, 3 or 4`,
+      );
+      app();
+    } else if (input === 1) {
+      choice = await rl.addTaskPrompt();
+      utils.addTodo(choice);
+      console.log(utils.readFileState);
+      utils.writeFile();
+      app();
+    } else if (input === 2) {
+      choice = await rl.removeTaskPrompt();
+      input = Number(choice);
+      utils.removeTodo(input);
+      utils.writeFile();
+      app();
+    } else if (input === 3) {
+      choice = await rl.updateTaskPrompt();
+      // console.log(choice);
+
+      // console.log(choice.taskID, choice.taskProperty, choice.taskPropertyValue);
+
+      input = Number(choice.taskID);
+      let property =
+        choice.taskProperty.charAt(0).toUpperCase() +
+        choice.taskProperty.slice(1).toLowerCase();
+      let edit = choice.taskPropertyValue;
+      utils.editTodo(input, property, edit);
+      utils.writeFile();
+      app();
+    } else if (input === 0) {
+      rl.exitApp();
+    }
   } else if (input === 0) {
     rl.exitApp();
   }
